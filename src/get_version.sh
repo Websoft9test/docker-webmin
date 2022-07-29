0fc437076f908144c6b0b2826b970e1e0b12e050
qiaofeng1227 <76487013@qq.com>
2022-07-29 11:02:47 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "webmin version:" $(docker exec -i $1 ls |grep webmin|cut -d- -f2) 1>> /data/logs/install_version.txt
+sudo echo "webmin version:" $(docker exec -i $1 cat /etc/webmin/version) |tee -a /data/logs/install_version.txt
